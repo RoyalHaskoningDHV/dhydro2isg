8fb73ff37f21eb2b6d7eba1263fbab03b87f30f8
Jeroen Winkelhorst <jeroen.winkelhorst@rhdhv.com>
2026-01-09 17:21:06 +0100

    removed hydrolib-core dependency and add reader functions

--- dhydro2isg/isc_files.py
+++ dhydro2isg/isc_files.py
@@ -1,8 +1,6 @@
 import geopandas as gpd
 import numpy as np
 import pandas as pd
-import rasterio
-from tqdm.auto import tqdm
 from shapely.geometry import Point, LineString
 from dhydro2isg.config import CRS_28992
 
@@ -24,25 +22,6 @@ def _coordinate_offset(offset, xy1, xy2, xy0=None):
     return [(xy1[0] + dx, xy1[1] + dy), (xy1[0] - dx, xy1[1] - dy)]
 
 
-def _distance_between_points(two_geometry_df, column1, column2):
-     """
-     calculates disctance between two points
-     """
-     distance = two_geometry_df[column1].distance(two_geometry_df[column2])
-     two_geometry_df["stretch"] = distance
-     return two_geometry_df
-
-
-def _is_left(df_line_point, line_col, point_col):
-     """
-     Checks if a point is left or right from a line using crossproduct
-     """
-     line_start_end = df_line_point[line_col].boundary
-     a, b = line_start_end.geoms[0], line_start_end.geoms[1]
-     c = df_line_point[point_col]
-     return ((b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)) > 0
-
-
 def is_left(line , point: Point):
     """
     Checks if a point is left or right from a line using crossproduct
@@ -52,19 +31,6 @@ def is_left(line , point: Point):
     a, b = line_start_end.geoms[0], line_start_end.geoms[1]
     c = point
     return ((b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)) > 0
-
-
-def _add_col_isleft(points, lines, line_col, point_col):
-    """
-    add columns to point dataframe that is 'True' is point is left of a line
-    """
-    joined_df = points.join(lines, how="left", rsuffix="lines", lsuffix="points")
-    is_left_outcome = joined_df.apply(
-        func=lambda x: _is_left(x, line_col=line_col, point_col=point_col), axis=1)
-    points["position"] = is_left_outcome.apply(lambda x: "left" if x==True else "right")
-    return points
-
-
 
 def isc_preprocess(raw_df, mrc):
     """
@@ -106,20 +72,6 @@ def isc_preprocess(raw_df, mrc):
     isc_clean_2 = isc_clean_2.astype(CROSS_SECTIONS_COLS)
     return isc_clean_2
 
-# # todo naming functions
-# def _create_ISC1_ISC2(w_segments, w_polygon, w_point, dem, offset_ics = 6, mrc = 1.12):
-#     dem = rasterio.open(dem, mode="r")
-#     intersection_points = to_intersectionpoint(w_segments, w_polygon, w_point, offset_ics=offset_ics)
-#     boundary_points = sample_points(dem, intersection_points)
-#     joined_df = boundary_points.join(w_point, how="left", rsuffix="mid", lsuffix="bound")
-#     df_with_distance = joined_df.apply(
-#         func=lambda x: _distance_between_points(x, column1="geometrybound", column2="geometrymid"),
-#         axis=1)
-#     df_with_left = _add_col_isleft(points=df_with_distance, lines=w_segments, line_col="geometry",point_col="geometrybound")
-
-#     isc = isc_preprocess(df_with_left, mrc=mrc)
-#     isc = isc_preprocess(df_with_left, mrc=mrc)
-#     return isc
 
 
 
